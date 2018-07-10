d1f536079a9b5444ae8c214488d7874e29acbe86
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-07-10 14:48:02 +0600

    updated metadata about the page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Sample Webpage | Ak&Co',
   },
   plugins: ['gatsby-plugin-react-helmet', `gatsby-plugin-sass`],
 }
