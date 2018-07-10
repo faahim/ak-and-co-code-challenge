0bbda09c99dc084f561c51a4bec06b1655c850a8
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-07-10 21:47:08 +0600

    implented cards component that generates cards based on provided data

---
+++ src/components/Cards.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from '../styles/SupportSection.module.scss'
+
+const Cards = ({ cardItems }) => (
+  <div className={styles.cardWrapper}>
+    {cardItems.map(item => (
+      <div className={styles.card} key={item.id}>
+        <a href={item.link}>{item.title}</a>
+      </div>
+    ))}
+  </div>
+)
+
+Cards.propTypes = {
+  cardItems: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default Cards
