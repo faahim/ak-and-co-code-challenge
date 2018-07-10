45fd75496cfd38f8b140a54ac58361d993deb0f9
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-07-11 01:03:13 +0600

    imported and rendered footer in layout index

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import Header from '../components/header'
+import Footer from '../components/Footer'
 import logo from '../imgs/logo.svg'
 import backgroundImage from '../imgs/header-bg.png'
 import './index.module.scss'
@@ -16,15 +17,14 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header logo={logo} backgroundImage={backgroundImage} />
-    <div>
-      {/* {console.log(`Muula ${JSON.stringify(data)}`)} */}
-      {children()}
-    </div>
+    <div>{children()}</div>
+    <Footer />
   </div>
 )
 
 Layout.propTypes = {
-  children: PropTypes.func,
+  data: PropTypes.objectOf(PropTypes.object).isRequired,
+  children: PropTypes.func.isRequired,
 }
 
 export default Layout
