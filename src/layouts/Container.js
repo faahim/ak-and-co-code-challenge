e85afa5b084972559c46c3e98b0070efac42e6ba
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-07-11 01:01:24 +0600

    created a common container cmoponent for consistency among all component

---
+++ src/layouts/Container.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styles from '../styles/Container.module.scss'
+
+const Container = ({ children, className, ...props }) => (
+  <div className={`${styles.root} ${className}`} {...props}>
+    {children}
+  </div>
+)
+
+Container.propTypes = {
+  children: PropTypes.node.isRequired,
+  className: PropTypes.string,
+}
+
+Container.defaultProps = {
+  className: '',
+}
+
+export default Container
