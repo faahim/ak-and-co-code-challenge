f0901db1cf4c9db2e9ed903367a9ab3eeb0e7d54
Afiur Rahman Fahim <afiur.fahim@gmail.com>
2018-07-11 12:36:23 +0600

    added modern normalize css for consistent default styling

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import 'modern-normalize/modern-normalize.css'
 import Header from '../components/header'
 import Footer from '../components/Footer'
 import logo from '../imgs/logo.svg'
